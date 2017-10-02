e2c1030eba0aa4d7503183ebbd57b77fc2035c9b
ssbhuvanesh <ssbhuvanesh@bitbucket.org>
2017-10-02 17:07:10 +0530

    gst bill server first commit & push

---
+++ GstBillServices/src/main/java/com/bhuvanesh/GstBillServicesApplication.java
@@ -0,0 +1,12 @@
+package com.bhuvanesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstBillServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstBillServicesApplication.class, args);
+	}
+}
---
+++ GstBillServices/src/main/java/com/bhuvanesh/gst_bill_controller/GstBillController.java
@@ -0,0 +1,5 @@
+package com.bhuvanesh.gst_bill_controller;
+
+public class GstBillController {
+
+}
