7a96f07d29c040f566765b6582d958237f5a356d
ssbhuvanesh <ssbhuvanesh@bitbucket.org>
2017-10-02 16:31:20 +0530

    new test proj 3 first commit

---
+++ NewTestProj3/src/main/java/com/example/test/NewTestProj3Application.java
@@ -0,0 +1,12 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTestProj3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewTestProj3Application.class, args);
+	}
+}
---
+++ NewTestProj3/src/main/java/com/example/test/TestController.java
@@ -0,0 +1,8 @@
+package com.example.test;
+
+public class TestController {
+
+	public void Test() {
+		// controller class receive request first.
+	}
+}
